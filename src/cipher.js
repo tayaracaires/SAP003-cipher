16af77f91adc3b234ad8639306ecc4b13307cf05
tayaracaires <tayaracaires@gmail.com>
2019-08-27 16:06:33 -0300

    Correção da função decode e organização do html

--- src/cipher.js
+++ src/cipher.js
@@ -1,43 +1,54 @@
 window.cipher = {
   encode: encode,
-  decode: decode 
+  decode: decode, 
+};
+
+function testaOffset (offset) {
+  if (offset < 0) {
+    while (offset < 0) {
+      offset +=26;
+    }
+  }
+  return offset;
 }
 
-function encode(offset, string){
+function encode (offset, string) {
+  offset = testaOffset(offset);
   let novaFrase = [];
   const tamanho = string.length;
-  for (let i=0; i<tamanho; i++){
+  for (let i=0; i<tamanho; i++) {
     let code = string[i].charCodeAt(0); 
-    if (code >= 65 && code <= 90){
+    if (code >= 65 && code <= 90) {
       let transforma = (code - 65 + offset) % 26 + 65;
       let result = String.fromCharCode(transforma);
       novaFrase.push(result);
-    }else if (code >=97 && code <= 122){
-        let transforma = (code - 97 + offset) % 26 + 97;
-        let result = String.fromCharCode(transforma);
-        novaFrase.push(result);
-    }else{
-      novaFrase.push(string[i])
+    } else if (code >=97 && code <= 122) {
+      let transforma = (code - 97 + offset) % 26 + 97;
+      let result = String.fromCharCode(transforma);
+      novaFrase.push(result);
+    } else {
+      novaFrase.push(string[i]);
     }
   }
   return novaFrase.join("");
 }
 
-function decode(offset, string){
+function decode(offset, string) {
+  offset = testaOffset(offset);
   let novaFrase = [];
   const tamanho = string.length;
-  for (let i=0; i<tamanho; i++){
+  for (let i=0; i<tamanho; i++) {
     let code = string[i].charCodeAt(0); 
-    if (code >= 65 && code <= 90){
+    if (code >= 65 && code <= 90) {
       let transforma = (code - 90 - offset) % 26 + 90;
       let result = String.fromCharCode(transforma);
       novaFrase.push(result);
-    }else if (code >=97 && code <= 122){
+    } else if (code >=97 && code <= 122) {
       let transforma = (code - 122 - offset) % 26 + 122;
       let result = String.fromCharCode(transforma);
       novaFrase.push(result);
-    }else{
-      novaFrase.push(string[i])
+    } else {
+      novaFrase.push(string[i]);
     }
   }
   return novaFrase.join("");
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-function pegaDados(){
-    event.preventDefault()
-    let string = document.getElementById("texto").value;
-    const offset = parseInt(document.getElementById("code").value);
-    let resultado = window.cipher.encode(offset, string);
-    document.getElementById("novaFrase").innerHTML=`<p>O texto codificado é: "${resultado}"</p>`
-    return resultado
-    }
-function enviaDados(){
-    event.preventDefault()
-    let string = pegaDados();
-    const offset = parseInt(document.getElementById("code").value);
-    let volta = window.cipher.decode(offset, string);
-    document.getElementById("voltaFrase").innerHTML=`<p>O texto decodificado é: "${volta}"</p>`
+function pegaDados() {
+  event.preventDefault();
+  let string = document.getElementById("texto").value;
+  const offset = parseInt(document.getElementById("code").value);
+  let resultado = window.cipher.encode(offset, string);
+  document.getElementById("novaFrase").innerHTML=`<p>A senha codificada é: "${resultado}"</p>`;
+}
+function enviaDados() {
+  event.preventDefault();
+  let string = document.getElementById("texto").value;
+  const offset = parseInt(document.getElementById("code").value);
+  let volta = window.cipher.decode(offset, string);
+  document.getElementById("voltaFrase").innerHTML=`<p>A senha decodificada é: "${volta}"</p>`;
 }
